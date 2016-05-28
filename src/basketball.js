3dc041aef01cde77eee9508dbe06cbee10b80b24
Arun <arun@in-arunsathin.local>
2016-05-28 17:44:05 +0530

    The fulfiled ball(goal) can be move to balls in basket(gone list)

--- src/basketball.js
+++ src/basketball.js
@@ -2,10 +2,16 @@ var app = angular.module("basketball", []);
 
 app.controller("myCtrl", function ($scope) {
     $scope.balls = ["one night at beach", "new bike", "a solo ride to vynad"];
+    $scope.basket = [];
     $scope.addBall = function () {
         $scope.balls.push($scope.addMe);
     }
     $scope.removeBall = function (x) {
         $scope.balls.splice(x, 1);
     }
+    $scope.moveToDone = function (x){
+        $scope.basket.push($scope.balls[x])
+        $scope.balls.splice(x, 1);
+    }
+
 });
